fcc4b9ac2ec1cd3e37757b54b629924ad390a8bb
Patrick <patrickkottwitz@gmail.com>
2021-09-06 13:22:32 -0300

    instalado chakra-ui

--- component/Layout.js
+++ component/Layout.js
@@ -1,3 +1,4 @@
+import { ChakraProvider } from '@chakra-ui/react'
 
 import Header from "./Header"
 import Footer from "./Footer"
@@ -5,10 +6,10 @@ import Footer from "./Footer"
 
 export default function Layout({ children }) {
   return (
-    <>
+    <ChakraProvider>
       <Header />
       <main>{children}</main>
       <Footer />
-    </>
+    </ChakraProvider>
   )
 }
